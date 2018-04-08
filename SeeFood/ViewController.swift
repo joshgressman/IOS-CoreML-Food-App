3dcec6ea83d0d785962374c82d74cb2108c029b0
joshgressman <joshgressman@gmail.com>
2018-04-08 15:36:30 -0500

    first commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Josh Gressman on 4/8/18.
+//  Copyright © 2018 Josh Gressman. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+      
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+         imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+    }
+
+   
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+
+}
+
