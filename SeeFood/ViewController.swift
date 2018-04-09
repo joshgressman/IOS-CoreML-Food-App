f954450c6fbe9e0edf05e2f09060e7e3132e935b
joshgressman <joshgressman@gmail.com>
2018-04-09 18:35:01 -0500

    added ML identifyer

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -29,10 +29,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
          imageView.image = userPickedImage
+        
+            guard let ciimage = CIImage(image: userPickedImage) else {fatalError("Could not convert to UIImage to CIIamge")}
+            
+            detect(image: ciimage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {fatalError("Loading CoreML Model Failed")
+            
+        }
+    
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {fatalError("model failed to process image")
+                
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            
+         try handler.perform([request])
+            
+        }
+        catch {
+            print("Error handling request \(error)")
+        }
         
     }
 
